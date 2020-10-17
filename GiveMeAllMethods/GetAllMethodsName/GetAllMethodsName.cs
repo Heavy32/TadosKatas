044e762352c2480b116047ae7d7fc093f7a72485
Max Gorankov <maksim.gorankov@gmail.com>
2020-10-17 12:46:14 +0500

    refactoring

--- GiveMeAllMethods/GetAllMethodsName/GetAllMethodsName.cs
+++ GiveMeAllMethods/GetAllMethodsName/GetAllMethodsName.cs
@@ -6,8 +6,11 @@ namespace GetAllMethodsName
     public class GetAllMethodsName
     {
         public static string[] GetMethodNames(object TestObject)
-             => TestObject == null
-                ? new string[0]
-                : TestObject.GetType().GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Select(m => m.Name).ToArray();
+             => TestObject?
+                .GetType()
+                .GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Select(m => m.Name)
+                .ToArray()
+                ?? new string[0];
     }
 }
