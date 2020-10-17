f9a36005eeb8fdf0c25dbbc739251a23f73fe9bd
Max Gorankov <maksim.gorankov@gmail.com>
2020-10-17 14:05:39 +0500

    FirstTry

---
+++ Order/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Order
+{
+    public class Order
+    {
+        static public string OrderText(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return "";
+
+            string[] output = new string[9];
+
+            var words = text.Split(' ');
+
+            foreach (var word in words)
+            {
+                foreach (var c in word)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        int position = (int)Char.GetNumericValue(c);
+                        output[position - 1] = word;
+                    }
+                    continue;
+                }
+            }
+            return MakeStringFromArray(output);
+        }
+
+        static private string MakeStringFromArray(string[] arr)
+        {
+            string str = "";
+
+            foreach (var item in arr.Where(w => w != null))
+            {
+                str += item + ' ';
+            }
+
+            return str.Remove(str.Length - 1);
+        }
+    }
+}
---
+++ OrderTests/OrderTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Order;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Tests
+{
+    public class OrderTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual("Thi1s is2 3a T4est", Order.OrderText("is2 Thi1s T4est 3a"));
+            Assert.AreEqual("Fo1r the2 g3ood 4of th5e pe6ople", Order.OrderText("4of Fo1r pe6ople g3ood th5e the2"));
+            Assert.AreEqual("", Order.OrderText(""));
+        }
+    }
+}
