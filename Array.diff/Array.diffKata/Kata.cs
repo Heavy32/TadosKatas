f67d1a6aa7a2374827db87dae4c9f51b29287c6c
Max Gorankov <maksim.gorankov@gmail.com>
2020-10-19 14:32:30 +0500

    Add kata

---
+++ Array.diff/Array.diffKata/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Array.diffKata
+{
+    public class Kata
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            return a.Where(x => !b.Contains(x)).ToArray();
+        }
+    }
+}
---
+++ Array.diff/Array.diffTests/KataTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Array.diffKata;
+
+namespace Array.diffKata.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void InArray12Array1OutArray2()
+        {
+            Assert.AreEqual(new int[] { 2 }, Kata.ArrayDiff(new int[] { 1, 2 }, new int[] { 1 }));
+
+        }
+
+        [Test]
+        public void InArray122Array1OutArray22()
+        {
+            Assert.AreEqual(new int[] { 2, 2 }, Kata.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 1 }));
+        }
+
+        [Test]
+        public void InArray122Array2OutArray1()
+        {
+            Assert.AreEqual(new int[] { 1 }, Kata.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 2 }));
+        }
+
+        [Test]
+        public void InArrayEmptyArray12OutArrayEmpty()
+        {
+            Assert.AreEqual(new int[] { }, Kata.ArrayDiff(new int[] { }, new int[] { 1, 2 }));
+        }
+
+        [Test]
+        public void InArray122ArrayEmptyOutArray122()
+        {
+            Assert.AreEqual(new int[] { 1, 2, 2 }, Kata.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { }));
+        }
+    }
+}
