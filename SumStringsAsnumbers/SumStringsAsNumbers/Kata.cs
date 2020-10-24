c11d68e1392a44d37ad6c1cabdddea6f62beb52a
Max Gorankov <maksim.gorankov@gmail.com>
2020-10-24 20:43:32 +0500

    add Sum strings

---
+++ SumStringsAsnumbers/SumStringsAsNumbers/Kata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SumStringsAsNumbersKata
+{
+    public class Kata
+    {
+        public static string SumStrings(string a, string b)
+        {
+            string output = "";
+            int cache = 0;
+
+            string big = a.Length > b.Length ? a : b;
+            string small = a.Length > b.Length ? b : a;
+
+            for (int i = big.Length - 1; i >= 0; i--)
+            {
+                int digit1 = (int)char.GetNumericValue(big[i]);
+                int digit2 =
+                    i - (big.Length - small.Length) >= 0
+                    ? (int)char.GetNumericValue(small[i - (big.Length - small.Length)])
+                    : 0;
+
+                int tempSum = digit1 + digit2 + cache;
+
+                int DigitThisIteration =
+                    (i == 0)
+                    ? tempSum
+                    : tempSum % 10;
+
+                cache = tempSum / 10;
+                output = DigitThisIteration.ToString() + output;
+            }
+
+            return output.TrimStart('0');
+        }
+    }
+}
---
+++ SumStringsAsnumbers/SumStringsAsNumbersKataTests/KataTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using SumStringsAsNumbersKata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumStringsAsNumbersKata.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("579", Kata.SumStrings("123", "456"));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("8842", Kata.SumStrings("8797", "45"));
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual("10367", Kata.SumStrings("800", "9567"));
+        }
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual("100", Kata.SumStrings("99", "1"));
+        }
+        [Test]
+        public void Test5()
+        {
+            Assert.AreEqual("8670", Kata.SumStrings("00103", "08567"));
+        }
+        [Test]
+        public void Test6()
+        {
+            Assert.AreEqual("5", Kata.SumStrings("", "5"));
+        }
+        [Test]
+        public void Test7()
+        {
+            Assert.AreEqual("712577413488402631964821329", Kata.SumStrings("712569312664357328695151392", "8100824045303269669937"));
+        }
+        [Test]
+        public void Test8()
+        {
+            Assert.AreEqual("131151201344081895336534324866", Kata.SumStrings("50095301248058391139327916261", "81055900096023504197206408605"));
+        }
+    }
+}
+
