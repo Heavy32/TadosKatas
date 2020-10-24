d762a4f76621c5e6edf323c572d616b2bcf8ae8b
Max Gorankov <maksim.gorankov@gmail.com>
2020-10-24 22:06:08 +0500

    Add braces kata

---
+++ ValidBraces/ValidBracesKata/Kata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidBracesKata
+{
+    public class Kata
+    {
+        public static bool ValidBraces(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            Dictionary<char, char> pairs = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' }
+            };
+
+            foreach (var item in input)
+            {
+                if (pairs.ContainsKey(item))
+                {
+                    stack.Push(item);
+                }
+                else if (pairs.ContainsValue(item))
+                {
+                    if (stack.Count == 0 || pairs[stack.Pop()] != item)
+                        return false;
+                }
+                else return false;
+            }
+
+            return stack.Count == 0;
+        }
+
+    }
+}
---
+++ ValidBraces/ValidBracesKataTests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using ValidBracesKata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidBracesKata.Tests
+{
+    public class KataTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, Kata.ValidBraces("()"));
+        }
+        [Test]
+        public void Test2()
+        {
+
+            Assert.AreEqual(false, Kata.ValidBraces("[(])"));
+        }
+    }
+}
