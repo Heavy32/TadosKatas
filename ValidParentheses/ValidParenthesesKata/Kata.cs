0b4d52420a9fb788436f46eb74a49450bdb9b10e
Max Gorankov <maksim.gorankov@gmail.com>
2020-10-24 21:47:09 +0500

    Add valid parenthese kata

---
+++ ValidParentheses/ValidParenthesesKata/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace ValidParenthesesKata
+{
+    public class Kata
+    {
+        public static bool ValidParentheses(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var item in input)
+            {
+                switch (item)
+                {
+                    case '(':
+                        stack.Push(item);
+                        break;
+
+                    case ')':
+                        if (stack.Count == 0)
+                            return false;
+                        if (stack.Pop() != '(')
+                            return false;
+                        break;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ ValidParentheses/ValidParenthesesKataTests/KataTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace ValidParenthesesKata.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void SampleTest1()
+        {
+            Assert.AreEqual(true, Kata.ValidParentheses("()"));
+        }
+
+        [Test]
+        public void SampleTest2()
+        {
+            Assert.AreEqual(false, Kata.ValidParentheses(")(((("));
+        }
+    }
+}
