088b10456d5a4d553e031bb23552c30800f033a4
Max Gorankov <maksim.gorankov@gmail.com>
2020-10-27 00:12:10 +0500

    Add vasly clerk kata

---
+++ Vasya-Clerk/Vasya-ClerkKata/Kata.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Vasya_ClerkKata
+{
+    public class Kata
+    {
+        public static string Tickets(int[] peopleInLine)
+        {
+            Dictionary<int, int> bills = new Dictionary<int, int>
+            {
+                { 25, 0 },
+                { 50, 0 },
+                { 100, 0 },
+            };
+
+            foreach (var item in peopleInLine)
+            {
+                if (item == 25)
+                {
+                    bills[25] += 1;
+                }
+                else if (item == 50)
+                {
+                    if (bills[25] > 0)
+                    {
+                        bills[50] += 1;
+                        bills[25] -= 1;
+                    }
+                    else
+                    {
+                        return "NO";
+                    }
+                }
+                else if (item == 100)
+                {
+                    if (bills[25] >= 1 && bills[50] >= 1)
+                    {
+                        bills[50] -= 1;
+                        bills[25] -= 1;
+                        bills[100] += 1;
+                    }
+                    else if (bills[25] >= 3)
+                    {
+                        bills[25] -= 3;
+                        bills[100] += 1;
+                    }
+                    else
+                    {
+                        return "NO";
+                    }
+                }
+            }
+
+            return "YES";
+        }
+    }
+}
---
+++ Vasya-Clerk/Vasya-ClerkKataTests/KataTests.cs
@@ -0,0 +1,120 @@
+﻿using NUnit.Framework;
+
+namespace Vasya_ClerkKata.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void Test1()
+        {
+            int[] peopleInLine = new int[] { 25, 25, 50, 50 };
+            Assert.AreEqual("YES", Kata.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] peopleInLine = new int[] { 25, 100 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int[] peopleInLine = new int[] { 25, 25, 25, 25, 25, 25, 25, 25, 25, 25 };
+            Assert.AreEqual("YES", Kata.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            int[] peopleInLine = new int[] { 50, 50, 50, 50, 50, 50, 50, 50, 50, 50 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test5()
+        {
+            int[] peopleInLine = new int[] { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test6()
+        {
+            int[] peopleInLine = new int[] { 25, 25, 25, 25, 50, 100, 50 };
+            Assert.AreEqual("YES", Kata.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test7()
+        {
+            int[] peopleInLine = new int[] { 25, 25 };
+            Assert.AreEqual("YES", Kata.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test8()
+        {
+            int[] peopleInLine = new int[] { 50, 100, 100 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test9()
+        {
+            int[] peopleInLine = new int[] { 25, 25, 25, 25, 25, 25, 25, 50, 50, 50, 100, 100, 100, 100 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test10()
+        {
+            int[] peopleInLine = new int[] { 25, 25, 50, 50, 100 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+        [Test]
+        public void Test11()
+        {
+            int[] peopleInLine = new int[] { 25, 25, 25, 25, 25, 100, 100 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+        [Test]
+        public void Test12()
+        {
+            int[] peopleInLine = new int[] { 25, 50, 25, 100, 25, 25, 50, 100, 25, 25, 25, 100, 25, 25, 50, 100, 25, 50, 25, 100, 25, 50, 50, 50 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+        [Test]
+        public void Test13()
+        {
+            int[] peopleInLine = new int[] { 25, 25, 25, 100, 25, 25, 25, 100, 25, 25, 50, 100, 25, 25, 50, 100, 50, 50 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+        [Test]
+        public void Test14()
+        {
+            int[] peopleInLine = new int[] { 25, 50, 25, 100, 25, 25, 50, 100, 25, 50, 25, 100, 50, 25 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+        [Test]
+        public void Test15()
+        {
+            int[] peopleInLine = new int[] { 25, 50, 100, 25, 25, 25, 50 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+        [Test]
+        public void Test16()
+        {
+            int[] peopleInLine = new int[] { 25, 50, 25, 50, 100, 25, 25, 50 };
+            Assert.AreEqual("NO", Kata.Tickets(peopleInLine));
+        }
+        [Test]
+        public void Test17()
+        {
+            int[] peopleInLine = new int[] { 25, 25, 50, 100 };
+            Assert.AreEqual("YES", Kata.Tickets(peopleInLine));
+        }
+    }
+}
