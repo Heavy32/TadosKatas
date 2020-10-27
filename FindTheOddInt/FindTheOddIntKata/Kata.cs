f086a6be1b50040187149aa50848aa675ea33a80
Max Gorankov <maksim.gorankov@gmail.com>
2020-10-27 12:53:25 +0500

    Add FindTheOddInt kata

---
+++ FindTheOddInt/FindTheOddIntKata/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace FindTheOddIntKata
+{
+    public class Kata
+    {
+        public static int Find_it(int[] seq)
+        {
+            return seq.GroupBy(x => x).Where(x => x.Count() % 2 != 0).First().Key;
+        }
+    }
+}
---
+++ FindTheOddInt/FindTheOddIntKataTests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using FindTheOddIntKata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindTheOddIntKata.Tests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void Find_itTest()
+        {
+            Assert.AreEqual(5, Kata.Find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+            Assert.AreEqual(-1, Kata.Find_it(new[] { 1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5 }));
+            Assert.AreEqual(5, Kata.Find_it(new[] { 20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5 }));
+            Assert.AreEqual(10, Kata.Find_it(new[] { 10 }));
+            Assert.AreEqual(10, Kata.Find_it(new[] { 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1 }));
+            Assert.AreEqual(1, Kata.Find_it(new[] { 5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10 }));
+        }
+    }
+}
